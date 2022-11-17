a7559043391794f78b33cf99174360b9d86cb5d8
Paulo Arcenio <pauloarcenio.cs@gmail.com>
2022-11-17 10:48:19 -0200

    Adicionado as classes, Curso, Mentoria, Conteudo(abstract),Dev

--- src/br/com/pauloarcenio/DesafioPOOJavaDIO.java
+++ src/br/com/pauloarcenio/DesafioPOOJavaDIO.java
@@ -7,11 +7,7 @@ public class DesafioPOOJavaDIO {
  
     public static void main(String[] args) {
         
-        Curso curso1 = new Curso("Java","Curso de java basico",8);
-        Curso curso2 = new Curso("JavaScript","Curso de JS basico",4);
         
-        System.out.println(curso1);
-        System.out.println(curso2);
         
     }
     
